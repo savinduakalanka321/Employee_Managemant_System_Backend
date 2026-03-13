6f447a57e45d60e8ba19964d3cc5aa3682144720
savindu damsith <savinduakalanka321@gmail.com>
2026-03-14 01:01:00 +0530

    employee controller

--- src/main/java/com/employee/employee/Entity/EmployeeEntity.java
+++ src/main/java/com/employee/employee/Entity/EmployeeEntity.java
@@ -16,6 +16,7 @@ public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String department;
